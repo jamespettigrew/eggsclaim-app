d62d191b0239a48380f4a509ec7f390a16277332
James Pettigrew <james@innovum.com.au>
2017-10-27 01:08:16 +1100

    Now displaying relevant information in log

---
+++ Eggsclaim/ValueConverters/PresenceConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Eggsclaim.ValueConverters
+{
+    public class PresenceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                var eggsPresent = (bool)value;
+                return (eggsPresent) ? "Eggs waiting!" : "Collected!";
+            }
+            return value;
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Eggsclaim/ValueConverters/TimestampConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Eggsclaim.ValueConverters
+{
+    public class TimestampConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime)
+            {
+                var timestamp = (DateTime)value;
+                return timestamp.ToLocalTime().ToString("hh:mm tt dddd, MMMM d");
+            }
+            return value;
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
